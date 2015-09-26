43378b2b5f38c91f9a255ce243d40fd9eb4d68e0
Daniel Howe <daniel@rednoise.org>
2015-09-26 16:37:15 +0800

    fix to tests, rename KWICModel

--- test/KnownIssues-tests.js
+++ test/KnownIssues-tests.js
@@ -1,23 +1,23 @@
 var runtests = function () {
-   
+
     RiTa.SILENT = 1;
-   
-    test("RiString.stripPunctuation(unicode)", function () { 
-    
+
+    test("RiString.stripPunctuation(unicode)", function () {
+
         var res = RiTa.stripPunctuation("����������`',;:!?)He,;:!?)([].#l\"\\!@$%&}<>|+$%&}<>|+=-_\\o}<>|+=-_\\/*{^");
         equal(res, "Hello");
     });
 
-    test("RiString.replaceWordAt()", function () { 
-    
+    test("RiString.replaceWordAt()", function () {
+
         var rs = new RiString("Who are you?");
         rs.replaceWordAt(2,"");    // nice! this too...
         //equal(rs.text(), "Who are?"); // strange case, not sure
         equal(rs.text(), "Who are ?");
     });
-    
-    test("RiText.replaceWordAt()", function () { 
-        
+
+    test("RiText.replaceWordAt()", function () {
+
         var rs = new RiText("Who are you?");
         rs.replaceWordAt(2,"");    // nice! this too...
         //equal(rs.text(), "Who are?"); // strange case, not sure
@@ -25,30 +25,30 @@ var runtests = function () {
     });
 
     test("RiGrammar.expandWith()", function () { //TODO: fix impl.
-        
+
         equal("fix impl.");
     });
-    
-    test("RiLexicon.getSyllables", function () { 
+
+    test("RiLexicon.getSyllables", function () {
          var lex = RiLexicon();
 
  		// Problem: All punct is stripped except the &
         var result = lex._getSyllables("@#$%&*()");
         var answer = "";
         equal(result, answer);
-        
+
    		// Problem: All punct is stripped except the &
         var result = lex._getPhonemes("@#$%&*()"); // ?
         var answer = "";
         equal(result, answer);
-        
+
    		// Problem: All punct is stripped except the &
         var result = lex._getStresses("@#$%&*()");  // ?
         var answer = "";
         equal(result, answer);
     });
-    
-    test("RiLexicon.rhymes", function () { 
+
+    test("RiLexicon.rhymes", function () {
 
         var lex = RiLexicon();
 
@@ -56,7 +56,16 @@ var runtests = function () {
         var result = lex.rhymes("savage");
         var answer = [ "average", "ravage", "cabbage" ];
         deepEqual(result, answer);
-    }); 
+    });
+
+    test("RiTa.conjugate", function () {
+
+      equal(RiTa.conjugate("make", {
+        tense: RiTa.PAST_TENSE,
+        number: RiTa.SINGULAR,
+        person: RiTa.FIRST_PERSON
+      }), "made");
+    });
 }
 
 if (typeof exports != 'undefined') runtests(); //exports.unwrap = runtests;
