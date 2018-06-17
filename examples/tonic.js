3ef983360f93e2ca16afc33c56fbf33d8ce9cd44
dhowe <dhowe@users.noreply.github.com>
2018-06-17 19:40:02 +0800

    removed canvas lib

--- examples/tonic.js
+++ examples/tonic.js
@@ -1,13 +1,7 @@
-var RiTa = require('rita'),
-  rs = RiTa.RiString("The elephant took a bite!"),
-  features = rs.features();
+var RiTa = require('rita');
+  
+var rs = RiTa.RiString("The elephant took a bite!");
+var features = rs.analyze();
 
-var Canvas = require('canvas'),
-  canvas = new Canvas(300, 200),
-  ctx = canvas.getContext('2d'),
-  y = 20;
+console.log(features);
 
-for (f in features)
-  ctx.fillText(f + ': ' + features[f], 20, y += 20);
-
-canvas.toBuffer();
